e93bf48bbb3de1cbb01c39d79dcb7333d377d6bc
Andres Melara <andres.melara.t@gmail.com>
2023-05-17 19:39:47 -0400

    Basic "skeleton" for App.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,20 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<Layout />} />)
+);
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      {" "}
+      <RouterProvider router={router} />{" "}
+    </>
+  );
 }
 
 export default App;

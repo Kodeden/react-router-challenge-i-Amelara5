5fc33c2b794ed3c29326fc41f2e6a2ace7836e8c
Andres Melara <andres.melara.t@gmail.com>
2023-05-25 20:16:20 -0400

    UseAsyncValue to load data & map to create list

--- src/components/users/userList.jsx
+++ src/components/users/userList.jsx
@@ -1,3 +1,13 @@
+import { useAsyncValue } from "react-router-dom";
+import User from "./user";
+
 export default function userList() {
-  return <></>;
+  const values = useAsyncValue();
+  return (
+    <ul>
+      {values.map((user) => (
+        <User key={user.id} user={user} />
+      ))}
+    </ul>
+  );
 }

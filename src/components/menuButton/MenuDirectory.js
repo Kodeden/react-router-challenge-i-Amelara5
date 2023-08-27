29444c3ca6044739e553401d0337d85d0f6d3d19
Andres Melara <andres.melara.t@gmail.com>
2023-08-26 23:58:51 -0400

    feat: :technologist: create a directory for the buttons

---
+++ src/components/menuButton/MenuDirectory.js
@@ -0,0 +1,8 @@
+import BackButton from "./backButton";
+import DeleteButton from "./deleteButton";
+import EditButton from "./editButton";
+
+function MenuDirectory() {
+  return { BackButton, EditButton, DeleteButton };
+}
+export default MenuDirectory;

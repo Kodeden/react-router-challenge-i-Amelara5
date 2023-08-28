703c21446e1c6ee8317df72ed6c649fae8b97186
Andres Melara <andres.melara.t@gmail.com>
2023-08-27 20:22:37 -0400

    Add buttons to user profile

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -2,6 +2,8 @@ import { Suspense } from "react";
 import { Await, useLoaderData } from "react-router-dom";
 import Loading from "../components/loading";
 import BackButton from "../components/menuButton/backButton";
+import DeleteButton from "../components/menuButton/deleteButton";
+import EditButton from "../components/menuButton/editButton";
 import UserProfile from "../components/users/userProfile";
 
 function Profile() {
@@ -15,7 +17,11 @@ function Profile() {
         </Await>
       </Suspense>
 
-      <BackButton />
+      <div className="flex flex-col items-center justify-center">
+        <BackButton />
+        <EditButton />
+        <DeleteButton />
+      </div>
     </>
   );
 }

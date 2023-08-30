8540617826997b9e53d2f6197a186774f96f6979
Andres Melara <andres.melara.t@gmail.com>
2023-08-29 20:17:04 -0400

    refactor: :recycle: Move the buttons to userProfile
    Moving the buttons to a "deeper" level makes accessing the information easier

--- src/components/users/userProfile.jsx
+++ src/components/users/userProfile.jsx
@@ -1,17 +1,25 @@
 import { useAsyncValue } from "react-router-dom";
+import BackButton from "../menuButton/backButton";
+import DeleteButton from "../menuButton/deleteButton";
+import EditButton from "../menuButton/editButton";
 
-function Profile({ setUserId }) {
+function Profile() {
   const user = useAsyncValue();
-
   return (
-    <div className="m-1 flex flex-col items-center justify-center">
-      {/* <div>{user.id}</div> */}
-      {setUserId(user.id)}
-      <div>{user.name}</div>
-      <div>{user.tel}</div>
-      <div>{user.email}</div>
-      <img src={user.img} alt="Profile" />
-    </div>
+    <>
+      <div className="m-1 flex flex-col items-center justify-center">
+        {/* <div>{user.id}</div> */}
+        <div>{user.name}</div>
+        <div>{user.tel}</div>
+        <div>{user.email}</div>
+        <img src={user.img} alt="Profile" />
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <BackButton />
+        <EditButton user={user} />
+        <DeleteButton userId={user.id} />
+      </div>
+    </>
   );
 }
 
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,28 +1,18 @@
-import { Suspense, useState } from "react";
+import { Suspense } from "react";
 import { Await, useLoaderData } from "react-router-dom";
 import Loading from "../components/loading";
-import BackButton from "../components/menuButton/backButton";
-import DeleteButton from "../components/menuButton/deleteButton";
-import EditButton from "../components/menuButton/editButton";
 import UserProfile from "../components/users/userProfile";
 
 function Profile() {
   const user = useLoaderData();
-  const [userId, setUserId] = useState("");
 
   return (
     <>
       <Suspense fallback={<Loading />}>
         <Await resolve={user}>
-          <UserProfile setUserId={setUserId} />
+          <UserProfile />
         </Await>
       </Suspense>
-
-      <div className="flex flex-col items-center justify-center">
-        <BackButton />
-        <EditButton />
-        <DeleteButton userId={userId} />
-      </div>
     </>
   );
 }

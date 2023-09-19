ea572d78e497cf8e53ac70b2e239d39a1028dac6
Andres Melara <andres.melara.t@gmail.com>
2023-09-19 17:39:01 -0400

    Remove button tag

--- src/components/menuButton/backButton.jsx
+++ src/components/menuButton/backButton.jsx
@@ -2,15 +2,8 @@ import { Link } from "react-router-dom";
 
 function BackButton() {
   return (
-    <Link to={"/"}>
-      <button
-        className="menuButton"
-        // onClick={() => {
-        //   submit({ value: "back", name: "intent" }, { method: "PATCH" });
-        // }}
-      >
-        Back
-      </button>
+    <Link to={"/"} className="menuButton">
+      Back
     </Link>
   );
 }

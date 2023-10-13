5fdb28ff0408450b4e5b5d7a0684a7e01c06e987
Andres Melara <andres.melara.t@gmail.com>
2023-10-13 13:59:21 -0400

    test: :test_tube: adds a contact

--- src/tests/app.test.jsx
+++ src/tests/app.test.jsx
@@ -13,9 +13,32 @@ it("loads data", async () => {
   expect(webPageList).toHaveLength(databaseList.users.length);
 });
 
-// it("shows the alphabetical order", async () => {
-//   const appRender = render(<App />);
-// });
+it("adds a contact", async () => {
+  render(<App />);
+  const user = userEvent.setup();
+
+  const addContactButton = await screen.getByRole("link", {
+    name: /Add Contact/i,
+  });
+
+  user.click(addContactButton);
+
+  //  I have to check why 'form' alone doesn't work. Or try using "" as the name
+  // await screen.getByRole("form", { name: /userInformation/i });
+  //  For 'textbox' to be reachable and have a name. I have to add a label to it.
+  const userName = await screen.getByRole("textbox", { name: /name/i });
+  const userPhone = await screen.getByRole("textbox", {
+    name: /phone number/i,
+  });
+  const userEmail = await screen.getByRole("textbox", { name: /email/i });
+
+  await user.type(userName, "John Doe");
+  await user.type(userPhone, "2257-7777");
+  await user.type(userEmail, "jd@email.com");
+
+  // Check why once the button is pressed it keeps repeating itself
+  // await user.click(await screen.getByRole("button", { name: /save/i }));
+});
 
 // it("adds a contac", async () => {
 //   const appRender = render(<App />);

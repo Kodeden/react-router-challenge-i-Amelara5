df50876e42021ac36672d476bdef4c9bfd85edc2
Manav Misra <manavm@visionwebsoft.com>
2023-10-24 09:26:18 -0500

    Prevent navigation with `null`

--- src/components/menuButton/backButton.jsx
+++ src/components/menuButton/backButton.jsx
@@ -3,15 +3,12 @@ import { Link, useNavigation } from "react-router-dom";
 function BackButton() {
   const navigation = useNavigation();
 
-  if (navigation.state === "loading") {
-    return (
-      <div to={"/"} className="menuButton">
-        Back
-      </div>
-    );
-  }
   return (
-    <Link to={"/"} className="menuButton">
+    <Link
+      // Don't do anything if we are in a loading state and the user clicks.
+      to={navigation.state === "loading" ? null : "/"}
+      className="menuButton"
+    >
       Back
     </Link>
   );

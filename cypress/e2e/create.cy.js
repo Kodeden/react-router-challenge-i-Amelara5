f4802454722836d782506217086868f5da65f829
Andres Melara <andres.melara.t@gmail.com>
2023-11-17 13:42:08 -0500

    First test with Cypress

--- cypress/e2e/create.cy.js
+++ cypress/e2e/create.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+it("adds a contact", () => {
+  cy.visit("http://localhost:5173")
+
+  cy.findByRole("link", {name: /Add Contact/i}).click()
+
+  cy.findByLabelText(/Name/i).type("Jane Doe")
+  cy.findByLabelText(/Phone number/i).type("123-456-7891")
+  cy.findByLabelText(/Email/i).type("jane@doe.com")
+
+  // cy.findByRole("button", {name: /Save/i}.click())
 })

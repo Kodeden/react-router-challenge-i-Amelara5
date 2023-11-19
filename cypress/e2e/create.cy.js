2ad32284c721e0ead03840e53c76fb90061dff45
Andres Melara <andres.melara.t@gmail.com>
2023-11-18 19:39:21 -0500

    test: :white_check_mark: adds a contact

--- cypress/e2e/create.cy.js
+++ cypress/e2e/create.cy.js
@@ -1,11 +1,25 @@
+const NEW_NAME = "Jane Doe";
+const NEW_EMAIL = "jane@doe.com";
+const NEW_PHONE = "123-456-7891";
+
 it("adds a contact", () => {
-  cy.visit("http://localhost:5173")
+  cy.intercept("GET", "/users", { fixture: "contacts.json" });
+  cy.intercept("POST", "/users", (req) => {
+    console.log(req.body);
+  }).as("createContact");
+  cy.visit("http://localhost:5173");
 
-  cy.findByRole("link", {name: /Add Contact/i}).click()
+  cy.findByRole("link", { name: /Add Contact/i }).click();
 
-  cy.findByLabelText(/Name/i).type("Jane Doe")
-  cy.findByLabelText(/Phone number/i).type("123-456-7891")
-  cy.findByLabelText(/Email/i).type("jane@doe.com")
+  cy.findByLabelText(/Name/i).type(NEW_NAME);
+  cy.findByLabelText(/Phone number/i).type(NEW_PHONE);
+  cy.findByLabelText(/Email/i).type(NEW_EMAIL);
 
-  // cy.findByRole("button", {name: /Save/i}.click())
-})
+  cy.findByRole("button", { name: /Save/i }).click();
+  cy.wait("@createContact").then(({ request }) => {
+    const { name, email, tel } = request.body;
+    expect(name).to.equal(NEW_NAME);
+    expect(email).to.equal(NEW_EMAIL);
+    expect(tel).to.equal(NEW_PHONE);
+  });
+});

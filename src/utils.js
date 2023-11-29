3b7fd9b43ee56e422f22269a80b85752e0091f78
Manav Misra <manavm@visionwebsoft.com>
2023-11-29 07:54:35 -0600

    feat: :necktie: add utility fxn. to alphabetize

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 function initLetterGroups() {
   return Array.from({ length: 26 }, (_, i) =>
-    String.fromCharCode(65 + i)
+    String.fromCharCode(65 + i),
   ).reduce((groups, letter) => {
     groups[letter] = [];
     return groups;
@@ -11,6 +11,30 @@ function getFirstLetterOfLastName(name) {
   const names = name.split(" ");
   return names[names.length - 1][0];
 }
+
+export const alphabetizeContactsByLastName = (contacts) => {
+  // Within a React app, it's best to avoid mutations for performance and other reasons.
+  const contactsCopy = structuredClone(contacts);
+
+  return contactsCopy.sort((a, b) => {
+    const aSplitName = a.name.split(" ");
+    const bSplitName = b.name.split(" ");
+
+    const aLastName = aSplitName[aSplitName.length - 1];
+    const bLastName = bSplitName[bSplitName.length - 1];
+
+    const lastNameComparison = aLastName.localeCompare(bLastName);
+
+    if (lastNameComparison !== 0) {
+      return lastNameComparison;
+    }
+
+    const aFirstName = aSplitName[0];
+    const bFirstName = bSplitName[0];
+
+    return aFirstName.localeCompare(bFirstName);
+  });
+};
 
 export const groupContactsByLetterUsingLastNames = (contacts) => {
   return contacts.reduce((groups, contact) => {
